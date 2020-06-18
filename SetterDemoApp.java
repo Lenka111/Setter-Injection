fade77093b2aafc5646fe814311d5831be552567
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-06-18 10:49:21 -0500

    Add files via upload

---
+++ SetterDemoApp.java
@@ -0,0 +1,23 @@
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+    public static void main(String[] args) {
+
+        //load the s[ring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve the bean from the spring container
+        CricketCoach theCoach = context.getBean("cricketCoach", CricketCoach.class);
+
+        // call methods on the bean
+        System.out.println(theCoach.getDailyFortune());
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getEmailAddress());
+        System.out.println(theCoach.getTeam());
+
+        //close context
+        context.close();
+    }
+}

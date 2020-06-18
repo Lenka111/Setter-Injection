dd7712a2f9455eef84a1f5145e96d7cbaaa125a6
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-06-18 10:53:40 -0500

    Update SetterDemoApp.java

--- SetterDemoApp.java
+++ SetterDemoApp.java
@@ -1,3 +1,4 @@
+//Elena Voinu
 package com.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,6 +15,8 @@ public class SetterDemoApp {
         // call methods on the bean
         System.out.println(theCoach.getDailyFortune());
         System.out.println(theCoach.getDailyWorkout());
+        
+        //we set the values in the .xml file
         System.out.println(theCoach.getEmailAddress());
         System.out.println(theCoach.getTeam());
 
